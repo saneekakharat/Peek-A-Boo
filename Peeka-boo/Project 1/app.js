cc91b3c65c71e739cea221e7aed215696c8ae442
saneekakharat <78270897+saneekakharat@users.noreply.github.com>
2021-10-13 22:43:36 -0700

    Add files via upload

---
+++ Peeka-boo/Project 1/app.js
@@ -0,0 +1,23 @@
+const closedFace = document.querySelector('.closed')
+const openFace = document.querySelector('.open')
+const show = document.querySelector('.show')
+
+
+closedFace.addEventListener('click', () => {
+    if(openFace.classList.contains('open')) {
+        openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+
+
+openFace.addEventListener('click', () => {
+    if(closedFace.classList.contains('closed')) {
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
+        
+    }
+});
+
+
+
